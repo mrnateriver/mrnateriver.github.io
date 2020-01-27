841c00ef0ca3091c6bc2594ef39b937fad36656f
mrnateriver <mr.nate.river@gmail.com>
2020-01-27 20:27:41 +0700

    Moved sources, added styles and HTML minification

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 /// (c) Evgenii Dobrovidov, 2020
 
+import '../styles/styles.scss'
+
 import { setContactDetails } from './contacts'
 import { calculateAge, calculateWorkPeriods } from './info'
 import { setupSectionsExpansion, setupStickyHeader } from './ui'
